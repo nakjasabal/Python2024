b031ff57c1f9fb804c55f276637d55a8bedb483f
nakjasabal <nakjasabal@naver.com>
2024-10-08 07:37:53 +0900

    임시파일생성

---
+++ temp/carClass.py
@@ -0,0 +1,12 @@
+class Car:
+  def __init__(self, color, speed):
+    self.color = color
+    self.speed = speed
+  def speedUp(self):
+    print(f"속도를 올립니다")
+  def speedDown(self):
+    print(f"속도를 내립니다")
+
+
+
+    
